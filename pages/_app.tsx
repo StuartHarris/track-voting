9f2ca19524a75876412480a754a7292a46f6ed58
Stuart Harris <stuart.harris@red-badger.com>
2020-09-19 09:26:07 +0100

    fetch canned query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
-import { AppProps } from "next/app";
+import React from "react";
+import { withUrqlClient, NextUrqlAppContext } from "next-urql";
+import NextApp, { AppProps } from "next/app";
+import fetch from "isomorphic-unfetch";
 
 import "../styles/globals.css";
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+// the URL to /api/graphql
+const GRAPHQL_ENDPOINT = `http://localhost:3000/api/graphql`;
 
-export default App;
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+App.getInitialProps = async (ctx: NextUrqlAppContext) => {
+  const appProps = await NextApp.getInitialProps(ctx);
+  return { ...appProps };
+};
+
+export default withUrqlClient((_ssrExchange, _ctx) => ({
+  url: GRAPHQL_ENDPOINT,
+  fetch,
+}))(
+  // @ts-ignore
+  App
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,42 @@
+import { useQuery } from "urql";
 import { Button, TextField } from "@material-ui/core";
-import { createClient, useQuery, Provider } from "urql";
 import { useForm } from "react-hook-form";
+import gql from "graphql-tag";
 
 import Layout from "../components/layout";
-
-const API = "http://localhost:3000/api/graphql";
-const client = createClient({ url: API });
 
 type FormData = {
   example: string;
   exampleRequired: string;
 };
 
-const SearchQuery = `
+const SearchQuery = gql`
   query($search: String!) {
-    masters(search: $search) { title }
+    masters(search: $search) {
+      title
+    }
   }
 `;
 
-const Masters = ({ search }) => {
-  const [result] = useQuery({
+type MastersData = {
+  masters: {
+    title: string;
+  }[];
+};
+
+const Masters: React.FC = () => {
+  const [{ data, fetching, error }, executeQuery] = useQuery<MastersData>({
     query: SearchQuery,
-    variables: { search },
-    pause: !search,
+    variables: { search: "Let's Rock" },
   });
-
-  const { data, fetching, error } = result;
-
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
 
   return (
     <div>
-      {data &&
-        data.masters.map((master, i) => <div key={i}>{master.title}</div>)}
+      {data?.masters.map((master, i) => (
+        <div key={i}>{master.title}</div>
+      ))}
     </div>
   );
 };
@@ -41,10 +44,9 @@ const Masters = ({ search }) => {
 export default function Home() {
   const { register, handleSubmit } = useForm<FormData>();
 
-  const search = "";
   const onSubmit = ({ search }) => {
     console.log(search);
-    search = search;
+    // executeQuery(search);
   };
 
   return (
@@ -57,9 +59,7 @@ export default function Home() {
           </Button>
         </form>
       </section>
-      <Provider value={client}>
-        <Masters search={search} />
-      </Provider>
+      <Masters />
     </Layout>
   );
 }
