8785900378042f89051c399f7ef4daa780fb25da
Stuart Harris <stuart.harris@red-badger.com>
2020-10-17 12:47:41 +0100

    add token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { withUrqlClient, NextUrqlAppContext } from "next-urql";
 import NextApp, { AppProps } from "next/app";
 import fetch from "isomorphic-unfetch";
 import { Container, CssBaseline } from "@material-ui/core";
+import Cookies from "cookies";
+import { v4 as uuidV4 } from "uuid";
 
 import "../styles/globals.css";
 
@@ -24,10 +26,27 @@ App.getInitialProps = async (ctx: NextUrqlAppContext) => {
   return { ...appProps };
 };
 
-export default withUrqlClient((_ssrExchange, _ctx) => ({
-  url: GRAPHQL_ENDPOINT,
-  fetch,
-}))(
+export default withUrqlClient((_ssrExchange, ctx) => {
+  if (ctx && ctx.req && ctx.res) {
+    const cookie = new Cookies(ctx.req, ctx.res);
+    // @ts-ignore
+    let token = cookie.get("token");
+    if (!token) {
+      token = uuidV4();
+      cookie.set("token", token);
+    }
+  }
+
+  return {
+    url: GRAPHQL_ENDPOINT,
+    fetch,
+    fetchOptions: {
+      headers: {
+        Authorization: "stu",
+      },
+    },
+  };
+})(
   // @ts-ignore
   App
 );
